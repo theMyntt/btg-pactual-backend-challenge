48f77e13e6a335eb75016c87ae0567f3f9201b77
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2025-04-18 16:59:02 -0300

    docs: to string option

--- src/order-processor-service/src/main/java/com/gabrielaraujo/order_processor_service/core/entities/ClientEntity.java
+++ src/order-processor-service/src/main/java/com/gabrielaraujo/order_processor_service/core/entities/ClientEntity.java
@@ -3,11 +3,13 @@ package com.gabrielaraujo.order_processor_service.core.entities;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.util.Optional;
 
 @Getter
 @Setter(AccessLevel.PROTECTED)
+@ToString
 public abstract class ClientEntity {
     private int id;
     private String name;
