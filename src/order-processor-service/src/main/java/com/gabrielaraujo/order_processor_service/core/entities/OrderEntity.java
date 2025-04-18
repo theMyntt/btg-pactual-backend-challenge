0a5915173c16170d3cd98b908dcb49a5a15d503d
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2025-04-18 17:19:59 -0300

    chore: order can be transformed in a string

--- src/order-processor-service/src/main/java/com/gabrielaraujo/order_processor_service/core/entities/OrderEntity.java
+++ src/order-processor-service/src/main/java/com/gabrielaraujo/order_processor_service/core/entities/OrderEntity.java
@@ -3,11 +3,13 @@ package com.gabrielaraujo.order_processor_service.core.entities;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.util.List;
 
 @Getter
 @Setter(AccessLevel.PROTECTED)
+@ToString
 public abstract class OrderEntity {
     private int id;
     private ClientEntity client;
