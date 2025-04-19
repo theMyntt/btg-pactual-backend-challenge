c446db035964c4cf5c368a44f6a04f96d2913462
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2025-04-18 21:53:32 -0300

    chore: finalPrice on core entity

--- src/order-processor-service/src/main/java/com/gabrielaraujo/order_processor_service/core/entities/OrderEntity.java
+++ src/order-processor-service/src/main/java/com/gabrielaraujo/order_processor_service/core/entities/OrderEntity.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @Getter
@@ -12,6 +13,7 @@ import java.util.List;
 @ToString
 public abstract class OrderEntity {
     private int id;
+    private BigDecimal finalPrice;
     private ClientEntity client;
     private List<ProductEntity> items;
 
