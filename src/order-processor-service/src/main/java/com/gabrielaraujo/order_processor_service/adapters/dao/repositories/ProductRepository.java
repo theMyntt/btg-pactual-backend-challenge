0f9c465945881fa1a822cc68b3b3691d15394073
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2025-04-18 21:54:31 -0300

    chore: findByName query on repository

--- src/order-processor-service/src/main/java/com/gabrielaraujo/order_processor_service/adapters/dao/repositories/ProductRepository.java
+++ src/order-processor-service/src/main/java/com/gabrielaraujo/order_processor_service/adapters/dao/repositories/ProductRepository.java
@@ -4,6 +4,9 @@ import com.gabrielaraujo.order_processor_service.adapters.dao.tables.ProductTabl
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ProductRepository extends JpaRepository<ProductTable, Integer> {
+    ProductTable findByName(String name);
 }
