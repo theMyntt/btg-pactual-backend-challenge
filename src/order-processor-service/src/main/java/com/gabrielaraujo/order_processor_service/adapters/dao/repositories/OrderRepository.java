f9ec3684c2ca407818d16be713bad9346f9d1ba7
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2025-04-18 21:15:30 -0300

    refactor: order repository name

--- src/order-processor-service/src/main/java/com/gabrielaraujo/order_processor_service/adapters/dao/repositories/OrderRepository.java
+++ src/order-processor-service/src/main/java/com/gabrielaraujo/order_processor_service/adapters/dao/repositories/OrderRepository.java
@@ -1,8 +1,9 @@
 package com.gabrielaraujo.order_processor_service.adapters.dao.repositories;
 
+import com.gabrielaraujo.order_processor_service.adapters.dao.tables.OrderTable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrderTable extends JpaRepository<OrderTable, Integer> {
+public interface OrderRepository extends JpaRepository<OrderTable, Integer> {
 }
