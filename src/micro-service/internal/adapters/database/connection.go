cd449e7f7b90aff0fa689d33a1956b6ea8db0e6f
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2025-12-17 15:31:02 -0300

    ci: mysql configuration

---
+++ src/micro-service/internal/adapters/database/connection.go
@@ -0,0 +1,28 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+)
+
+func NewConnection() *sql.DB {
+	mysqlUser := os.Getenv("MYSQL_USER")
+	mysqlPassword := os.Getenv("MYSQL_PASSWORD")
+	mysqlUrl := os.Getenv("MYSQL_URL")
+	mysqlDb := os.Getenv("MYSQL_DATABASE")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s", mysqlUser, mysqlPassword, mysqlUrl, mysqlDb)
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	return db
+}
