ed80e594b579f2baa24cc9306ad88c69e428b98a
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2025-12-17 15:36:49 -0300

    refactor: recive mysql connection

--- src/micro-service/cmd/service/main.go
+++ src/micro-service/cmd/service/main.go
@@ -1,19 +1,24 @@
 package main
 
 import (
+	"github.com/theMyntt/btg-pactual-backend-challenge/internal/adapters/database"
 	internalQueue "github.com/theMyntt/btg-pactual-backend-challenge/internal/adapters/queue"
 )
 
 func main() {
 	queue, channel := internalQueue.NewConnection()
+	db := database.NewConnection()
+
 	messages, err := channel.Consume(queue.Name, "", true, false, false, false, nil)
 	if err != nil {
 		panic(err)
 	}
 
+	orderProcessorQueue := internalQueue.NewOrderProcessorQueueHandler(db)
+
 	go func() {
 		for d := range messages {
-			internalQueue.OrderProcessorQueueHandler(d.Body)
+			orderProcessorQueue.Run(d.Body)
 		}
 	}()
 }
--- src/micro-service/internal/adapters/queue/order_processor.go
+++ src/micro-service/internal/adapters/queue/order_processor.go
@@ -1,13 +1,28 @@
 package queue
 
 import (
+	"database/sql"
 	"encoding/json"
 	"log"
 
 	"github.com/theMyntt/btg-pactual-backend-challenge/internal/core/entities"
 )
 
-func OrderProcessorQueueHandler(body []byte) {
+type OrderProcessorHandler interface {
+	Run(body []byte)
+}
+
+type orderProcessorHandlerDependencies struct {
+	db *sql.DB
+}
+
+func NewOrderProcessorQueueHandler(db *sql.DB) OrderProcessorHandler {
+	return &orderProcessorHandlerDependencies{
+		db: db,
+	}
+}
+
+func (o *orderProcessorHandlerDependencies) Run(body []byte) {
 	var order entities.Order
 	err := json.Unmarshal(body, &order)
 	if err != nil {
